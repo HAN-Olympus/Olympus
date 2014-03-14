9f2390d29a52b3c1d71a22ec5c2eb4a7cec7bbce
Stephan Heijl <me@stephanheijl.com>
2014-03-15 00:39:32 +0100

    Working homepage

--- olympus.py
+++ olympus.py
@@ -15,21 +15,21 @@ def index():
 def loadCss(filename):
 	if os.path.isfile("css/%s" % filename):
 		cssFile = open("css/%s" % filename)
-	elif os.path.isfile("vendor/assets/stylesheets/%s" % filename):
-		cssFile = open("vendor/assets/stylesheets/%s" % filename)
+	elif os.path.isfile("bootstrap/css/%s" % filename):
+		cssFile = open("bootstrap/css/%s" % filename)
 	else:
-		abort(404)		
-		
+		abort(404)
+
 	css = cssFile.read()
 	cssFile.close()
 	return Response(css, mimetype="text/css")
-	
+
 @app.route("/js/<filename>")
 def loadJs(filename):
 	if os.path.isfile("js/%s" % filename):
 		jsFile = open("js/%s" % filename)
-	elif os.path.isfile("vendor/assets/javascripts/%s" % filename):
-		jsFile = open("vendor/assets/javascripts/%s" % filename)
+	elif os.path.isfile("bootstrap/js/%s" % filename):
+		jsFile = open("bootstrap/js/%s" % filename)
 	else:
 		abort(404)
 		
@@ -37,30 +37,26 @@ def loadJs(filename):
 	jsFile.close()
 	return Response(js, mimetype="application/javascript")
 	
-@app.route("/pdb/<filename>")
-def loadPdb(filename):
-	print filename
-
-	if re.match("^[a-zA-Z0-9]{1,4}\.pdb$", filename):
-		if os.path.isfile("pdb/%s" % filename):
-			pdbFile = open("pdb/%s" % filename)
-		else:
-			abort(404)		
-			
-		pdb = pdbFile.read()
-		pdbFile.close()
-	
-	elif re.match("^([^J][0-9BCOHNSOPrIFla@+\-\[\]\(\)\\\/%=#$,.~&!]{3,})$", filename, flags=re.IGNORECASE):
-		pdb = convertSmilesToPDB(str(filename).strip("pdb/"))
-		
+@app.route("/fonts/<filename>")
+def loadFont(filename):
+	if os.path.isfile("fonts/%s" % filename):
+		fontFile = open("fonts/%s" % filename)
+	elif os.path.isfile("bootstrap/js/%s" % filename):
+		fontFile = open("bootstrap/js/%s" % filename)
 	else:
-		abort(404);
-	
-	return Response(pdb, mimetype="chemical/x-pdb")
+		abort(404)
+		
+	font = fontFile.read()
+	fontFile.close()
+	return Response(font, mimetype="application/javascript")	
 
 @app.route("/<filename>")
 def loadPage(filename):
-	return render_template(filename, name=filename)
+	if os.path.isfile("template/%s" % filename):
+		return render_template(filename, name=filename)
+	else:
+		abort(404)
+		return False;
 	
 		
 # TESTING #
@@ -71,9 +67,9 @@ def test_loadJs():
 def test_loadCss():
 	assert loadCss("style.css"), "Could not load necessary CSS file. There is something wrong with the loadCss method."
 
-def test_loadPdb():
-	assert loadPdb("I3.pdb"), "Could not load test PDB file. There is something wrong with the loadPdb method."
-	assert loadPdb("CCc1nn(C)c2c(=O)[nH]c(nc12)c3cc(ccc3OCC)S(=O)(=O)N4CCN(C)CC4"), "Could not load SMILES sequence."
+def test_loadFont():
+	assert loadFont("glyphicons-halflings-regular.woff"), "Could not load necessary Font file. There is something wrong with the loadFont method."
+
 	
 if __name__ == "__main__":
     app.run()
