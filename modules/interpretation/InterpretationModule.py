64174c55f7585606ed4a8217d4d7aa2dfbfd3352
Stephan Heijl <me@stephanheijl.com>
2014-03-30 05:23:52 +0200

    Added generic modules to make sure folders get uploaded and test passes, added more advanced carousel to webapp.

---
+++ modules/interpretation/InterpretationModule.py
@@ -0,0 +1,22 @@
+""" Interpretation base class 
+Abstract class that provides base classes and access to classes in the ../lib directory
+Uses part of the additionalImport script to allow access to the lib classes.
+Refer to the page on Interpretation modules for a list of modules distributed with this version of Olympus.
+"""
+# Add the ../lib directory to the system path
+import os, sys
+currentDir = os.path.dirname(__file__)
+relLibDir = currentDir + "/../../lib"
+absLibDir = os.path.abspath(relLibDir)
+sys.path.insert(0, absLibDir)
+
+# Library classes are now accessible
+import Module
+import datetime
+
+class InterpretationModule(Module.Module):
+	""" Base class for all interpretation modules. Provides some generic methods. """
+	
+	def __init__(self):
+		""" Does nothing. """
+		pass
