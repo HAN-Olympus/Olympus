037ae32c0c703c871dd4767c44bfe10f668c2b2d
Stephan Heijl <me@stephanheijl.com>
2014-05-08 03:38:37 +0200

    Output added

---
+++ lib/Output.py
@@ -0,0 +1,12 @@
+import StoredObject
+
+class Output(StoredObject.StoredObject):
+	def __init__(self):
+		self.job_id = 0
+		super(Output, self).__init__(database = "olympus", collection = "output")
+		
+	def getByJobId(self, id):
+		print id
+		results = self.getObjectsByKey("job_id", id)
+		print results
+		return results
