b731366eec83b3e85cf1ae12ce0670f2e7bc04a2
Stephan Heijl <me@stephanheijl.com>
2014-06-30 01:37:45 +0200

    Fixed Gearman command

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Olympus documentation build configuration file, created by
-# sphinx-quickstart on Mon Jun 30 00:24:00 2014.
+# sphinx-quickstart on Mon Jun 30 01:32:01 2014.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
