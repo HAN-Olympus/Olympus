fcd2dec56858033402a3e421bb564f4d9c4d7f37
Stephan Heijl <me@stephanheijl.com>
2014-08-12 01:01:38 +0200

    Restored compiling.js

---
+++ Olympus/webapp/js/compiling.js
@@ -0,0 +1,48 @@
+$(function () {
+	// Instantiate sigma.js and customize rendering :
+	var sigInst = sigma.init(document.getElementById('sigma-container')).drawingProperties({
+		defaultLabelColor: '#fff',
+		defaultLabelSize: 14,
+		defaultLabelBGColor: '#fff',
+		defaultLabelHoverColor: '#000',
+		labelThreshold: 6,
+		defaultEdgeType: 'direct',
+		defaultEdgeArrow: 'target',
+		defaultEdgeColor: 'default',
+		defaultNodeColor: 'default'
+	}).graphProperties({
+		minNodeSize: 2,
+		maxNodeSize: 7,
+		minEdgeSize: 2,
+		maxEdgeSize: 2
+	}).mouseProperties({
+		maxRatio: 32
+	});
+
+	// Parse a GEXF encoded file to fill the graph
+	// (requires "sigma.parseGexf.js" to be included)
+	sigInst.parseGexf('/gexf/' + window.location.search);
+
+	sigInst.iterNodes(function (n) {
+		n.size += (n.inDegree + n.outDegree)
+	})
+
+	// Draw the graph :
+	sigInst.draw();
+
+	// Start the Force Atlas Algorithm
+	sigInst.startForceAtlas2();
+
+	//
+	$.getJSON("/compiler/" + window.location.search, function (response) {
+		console.log(response);
+		$(".btn.download").attr("disabled", false).attr("href", "/downloadCompiled/" + response["id"])
+		$("h2").append("Done.")
+		// We can stop drawing a little after when we're done compiling.
+		setTimeout(function () {
+			sigInst.stopForceAtlas2();
+		}, 3000);
+	});
+
+
+});
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Olympus documentation build configuration file, created by
-# sphinx-quickstart on Mon Aug 11 17:03:08 2014.
+# sphinx-quickstart on Mon Aug 11 17:25:36 2014.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
